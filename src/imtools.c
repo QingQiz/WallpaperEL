6af2a75531338d22e3dc74c7e47a6c2cf6847a20
QingQiz <sofeeys@outlook.com>
2020-01-14 02:03:22 +0800

    feat: fifo support for non-rgba image & catch BadPixmap exception

--- src/imtools.c
+++ src/imtools.c
@@ -2,6 +2,7 @@
 
 #include "imtools.h"
 #include "debug.h"
+#include "options.h"
 
 Display *disp = NULL;
 Visual *vis = NULL;
@@ -13,9 +14,16 @@ Window root = 0;
 WEMonitor *monitor_l;
 int monitor_n;
 
-static Imlib_Image im_alpha = NULL;
+static int X_error_handler(Display *d, XErrorEvent *e) {
+    if (e->error_code == BadPixmap) {
+        assert(!opts.fifo, "Bad Pixmap, try running without --fifo");
+    }
+    return 0;
+}
 
 void init_x_and_imtools() {
+    XSetErrorHandler(X_error_handler);
+
     disp = XOpenDisplay(NULL);
     assert(disp, "Can't open X display");
 
@@ -34,10 +42,6 @@ void init_x_and_imtools() {
     wmDeleteWindow = XInternAtom(disp, "WM_DELETE_WINDOW", False);
 
     imlib_set_cache_size(4 * 1024 * 1024);
-
-    im_alpha = imlib_create_image(scr->width, scr->height);
-    imlib_context_set_image(im_alpha);
-    imlib_image_set_has_alpha(1);
 
     WEGetMonitorList(disp, root, &monitor_l, &monitor_n);
 
@@ -65,18 +69,37 @@ int image_get_height(Imlib_Image im) {
    return imlib_image_get_height();
 }
 
-void image_set_alpha(Imlib_Image im, int alpha) {
+Imlib_Image image_set_alpha(Imlib_Image im, int alpha) {
+    static Imlib_Image im_aph = 0, im_cpy = 0;
+    if (im_aph) image_free(im_aph);
+    if (im_cpy) image_free(im_cpy);
+
     imlib_context_set_image(im);
     int h = imlib_image_get_height();
     int w = imlib_image_get_width();
 
-    imlib_context_set_image(im_alpha);
+    im_aph = imlib_create_image(w, h);
+    imlib_context_set_image(im_aph);
+    imlib_image_set_has_alpha(1);
     imlib_context_set_color(0, 0, 0, alpha);
     imlib_image_fill_rectangle(0, 0, w, h);
 
-    imlib_context_set_image(im);
+    im_cpy = imlib_create_image(w, h);
+    imlib_context_set_image(im_cpy);
     imlib_image_set_has_alpha(1);
-    imlib_image_copy_alpha_to_image(im_alpha, 0, 0);
+    imlib_blend_image_onto_image(im, 1, 0, 0, w, h, 0, 0, w, h);
+    imlib_image_copy_alpha_to_image(im_aph, 0, 0);
+
+    // static int cnt = 0;
+    // char s[100];
+    // sprintf(s, "frame_%02d.png", cnt++);
+    // imlib_save_image(s);
+    return im_cpy;
+}
+
+void image_free(Imlib_Image im) {
+    imlib_context_set_image(im);
+    imlib_free_image();
 }
 
 void render_image_part_on_drawable_at_size(
