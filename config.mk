39b9af8d5dcd3c054919b8e44dd0df8b8946a9a9
QingQiz <sofeeys@outlook.com>
2020-01-20 20:49:31 +0800

    chore: update Makefile use default cc

--- config.mk
+++ config.mk
@@ -1,4 +1,12 @@
 EXEC_NAME = we
-VERSION = v0.2.2
+VERSION   = v0.2.2
+
+
+CFLAGS   = -Wall -Wextra -c \
+	   -DPACKAGE=\"$(EXEC_NAME)\" \
+	   -DVERSION=\"$(VERSION)\"
+
+LDLIBS   = -lX11 -lXrandr -lImlib2
+
 
 INSTALL_DIR = /usr/bin
