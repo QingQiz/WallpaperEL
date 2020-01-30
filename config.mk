4a0711cbbd37950c6b941a7d7d608c274f84d811
QingQiz <sofeeys@outlook.com>
2020-01-30 20:39:05 +0800

    chore: compile with -O2

--- config.mk
+++ config.mk
@@ -2,7 +2,7 @@ EXEC_NAME = we
 VERSION   = $(shell git tag | tail -n1)
 
 
-CFLAGS   = -Wall -Wextra -c \
+CFLAGS   = -Wall -Wextra -c -O2 \
 	   -DPACKAGE=\"$(EXEC_NAME)\" \
 	   -DVERSION=\"$(VERSION)\"
 
