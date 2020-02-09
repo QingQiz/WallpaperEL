4e84d2a924c88c3085e7570190b9d8af1d3b4618
QingQiz <sofeeys@outlook.com>
2020-02-10 04:00:35 +0800

    chore: update Makefile

--- config.mk
+++ config.mk
@@ -1,12 +1,18 @@
-EXEC_NAME = we
-VERSION   = $(shell git tag | tail -n1)
+EXEC_NAME   = we
+INSTALL_DIR = /usr/bin
+
+SHELL       = /usr/bin/env bash
+VERSION     = $(shell git tag | tail -n1)
 
 
 CFLAGS   = -Wall -Wextra -c -O2 \
 	   -DPACKAGE=\"$(EXEC_NAME)\" \
 	   -DVERSION=\"$(VERSION)\"
 
-LDLIBS   = -lX11 -lXrandr -lImlib2
+LDLIBS   = -lX11 -lXrandr -lImlib2 -lasound -lpthread
 
+little_endian = $(shell [[ `lscpu | grep "Byte Order" | grep "Little"` ]] && echo 1 || echo 0)
 
-INSTALL_DIR = /usr/bin
+ifeq ($(little_endian), 1)
+	CFLAGS += -DLITTLE_ENDIAN
+endif
