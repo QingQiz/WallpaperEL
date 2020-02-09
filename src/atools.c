22c6c4750571bed639f627e6cbf8299378062b4c
QingQiz <sofeeys@outlook.com>
2020-02-10 03:58:41 +0800

    feat: parsing wav

---
+++ src/atools.c
@@ -0,0 +1,219 @@
+#include <string.h>
+#include <signal.h>
+
+#include "atools.h"
+#include "debug.h"
+
+snd_pcm_t *handler;
+snd_pcm_hw_params_t *params;
+
+char *org_data = NULL;
+size_t org_size;
+
+char *pcm_data = NULL;
+size_t pcm_size;
+
+audio_header_t header;
+
+static char WEAtoolsReadDataFromPcm(void *p, size_t size) {
+    static char *position = NULL;
+    char retval = 0;
+    char *ptr = (char*)p;
+    if (position == NULL) position = pcm_data;
+    while (size--) {
+        if (position >= pcm_data + pcm_size) {
+            // D("EOF in pcm");
+            retval = 1;
+            position = pcm_data;
+            break;
+        }
+        *(ptr++) = *(position++);
+    }
+    return retval;
+}
+
+static char WEAtoolsReadDataFromOrg(void *p, size_t size) {
+    static char *position = NULL;
+    char retval = 0;
+    char *ptr = (char*)p;
+    if (position == NULL) position = org_data;
+    while (size--) {
+        if (position >= org_data + org_size) {
+            D("EOF in header");
+            retval = 1;
+            position = org_data;
+            break;
+        }
+        *(ptr++) = *(position++);
+    }
+    return retval;
+}
+
+static void WEAtoolsLoadWaveHeader() {
+    static unsigned char buf[4];
+    wave_header_t *h = &header.h.wave_header;
+
+    // 1-4: RIFF
+    WEAtoolsReadDataFromOrg(h->riff, 4);
+    assert(!strncmp("RIFF", h->riff, 4), "RIFF descriptor not found");
+    // 5-8: RIFF size
+    WEAtoolsReadDataFromOrg(buf, 4);
+    h->overall_size = BUF2NUM4(buf);
+    // 9-12: WAVE
+    WEAtoolsReadDataFromOrg(h->wave, 4);
+    assert(!strncmp("WAVE", h->wave, 4), "WAVE chunk ID not found");
+    // 13-16: fmt
+    WEAtoolsReadDataFromOrg(h->fmt_chunk_marker, 4);
+    assert(!strncmp("fmt", h->fmt_chunk_marker, 3), "fmt chunk format not found");
+    // 17-20: format size
+    WEAtoolsReadDataFromOrg(buf, 4);
+    h->length_of_fmt = BUF2NUM4(buf);
+    // 21-22: format type
+    WEAtoolsReadDataFromOrg(buf, 2);
+    h->format_type = BUF2NUM2(buf);
+    assert(h->format_type == PCM, "Only supports PcM");
+    // 23-24: num channels
+    WEAtoolsReadDataFromOrg(buf, 2);
+    h->channels = BUF2NUM2(buf);
+    // 25-28: sample rate
+    WEAtoolsReadDataFromOrg(buf, 4);
+    h->sample_rate = BUF2NUM4(buf);
+    // 29-32: byte rate
+    WEAtoolsReadDataFromOrg(buf, 4);
+    h->byterate = BUF2NUM4(buf);
+    // 33-34: block align
+    WEAtoolsReadDataFromOrg(buf, 2);
+    h->block_align = BUF2NUM2(buf);
+    // 35-36: bits per sample
+    WEAtoolsReadDataFromOrg(buf, 2);
+    h->bits_per_sample = BUF2NUM2(buf);
+    // data
+    do {
+        // chunk id
+        WEAtoolsReadDataFromOrg(h->data_chunk_header, sizeof(h->data_chunk_header));
+        // size
+        WEAtoolsReadDataFromOrg(buf, 4);
+        h->data_size = BUF2NUM4(buf);
+
+        if (strncmp("data", h->data_chunk_header, 4) != 0) {
+            while (h->data_size--) WEAtoolsReadDataFromOrg(buf, 1);
+        } else break;
+    } while (1);
+
+    header.data_size = h->data_size;
+    header.channels = h->channels;
+    header.data_rate = h->data_size;
+    header.sample_rate = h->sample_rate;
+    header.bits_per_sample = h->bits_per_sample;
+    header.duration = (double)h->overall_size / h->byterate;
+    header.num_samples = h->data_size * 8 / (h->channels * h->bits_per_sample);
+    header.sample_size = h->channels * h->bits_per_sample;
+    header.type = WAVE;
+    assert(header.sample_size % header.channels == 0, "wrong sample size");
+}
+
+static void WEAtoolsLoadFile(char *filename) {
+    FILE *f = fopen(filename, "rb");
+    assert(f, "unable to load file: %s", filename);
+
+    fseek(f, 0, SEEK_END);
+    org_size = ftell(f);
+    rewind(f);
+
+    org_data = (char*)malloc(org_size + 1);
+    assert(org_data != NULL, "unable to alloc memory");
+    assert(fread(org_data, org_size, 1, f) == 1, "entire read fails");
+
+    fclose(f);
+}
+
+static void WEAtoolsReadFileHeader(char *filename) {
+    int i = 0;
+    while (filename[i++] != 0);
+    while (i > 0 && filename[--i] != '.');
+    char *file_ext = filename + i + 1;
+    
+    if (strcmp(file_ext, "wav") == 0) {
+        WEAtoolsLoadWaveHeader();
+    } else if (strcmp(file_ext, "mp3") == 0) {
+        header.type = MP3;
+    } else if (strcmp(file_ext, "flac") == 0) {
+        header.type = FLAC;
+    } else if (strcmp(file_ext, "m4a") == 0) {
+        header.type = ALAC;
+    }
+    // TODO
+    assert(header.type == WAVE, "only supports wav")
+}
+
+static void WEAtoolsLoadPcmData() {
+    pcm_size = header.data_size;
+    pcm_data = (char*)malloc(header.data_size);
+    WEAtoolsReadDataFromOrg(pcm_data, pcm_size);
+}
+
+void WEAtoolsInit(char *filename) {
+    WEAtoolsLoadFile(filename);
+    WEAtoolsReadFileHeader(filename);
+    int rc, dir = 0;
+    // open pcm device for playback
+    rc = snd_pcm_open(&handler, "default", SND_PCM_STREAM_PLAYBACK, 0);
+    assert(rc >= 0, "unable to open pcm device: %s", snd_strerror(rc));
+
+    // allocate a hardware parameters object and fill it in with default values
+    snd_pcm_hw_params_alloca(&params);
+    snd_pcm_hw_params_any(handler, params);
+    snd_pcm_hw_params_set_access(handler, params, SND_PCM_ACCESS_RW_INTERLEAVED);
+#ifdef LITTLE_ENDIAN
+    if (header.bits_per_sample == 8) {
+        snd_pcm_hw_params_set_format(handler, params, SND_PCM_FORMAT_S8);
+    } else if (header.bits_per_sample == 16) {
+        snd_pcm_hw_params_set_format(handler, params, SND_PCM_FORMAT_S16_LE);
+    } else if (header.bits_per_sample == 24) {
+        snd_pcm_hw_params_set_format(handler, params, SND_PCM_FORMAT_S24_LE);
+    } else if (header.bits_per_sample == 32) {
+        snd_pcm_hw_params_set_format(handler, params, SND_PCM_FORMAT_S32_LE);
+    }
+#else
+    if (header.bits_per_sample == 8) {
+        snd_pcm_hw_params_set_format(handler, params, SND_PCM_FORMAT_S8);
+    } else if (header.bits_per_sample == 16) {
+        snd_pcm_hw_params_set_format(handler, params, SND_PCM_FORMAT_S16_BE);
+    } else if (header.bits_per_sample == 24) {
+        snd_pcm_hw_params_set_format(handler, params, SND_PCM_FORMAT_S24_BE);
+    } else if (header.bits_per_sample == 32) {
+        snd_pcm_hw_params_set_format(handler, params, SND_PCM_FORMAT_S32_BE);
+    }
+#endif
+    snd_pcm_hw_params_set_channels(handler, params, header.channels);
+    snd_pcm_hw_params_set_rate(handler, params, header.sample_rate, dir);
+    snd_pcm_hw_params_set_period_size(handler, params, header.sample_size, dir);
+
+    // write the parameters to the driver
+    rc = snd_pcm_hw_params(handler, params);
+    assert(rc >= 0, "unable to set hw parameters: %s", snd_strerror(rc));
+
+    WEAtoolsLoadPcmData();
+}
+
+void WEAtoolsDestruct() {
+    snd_pcm_drain(handler);
+    snd_pcm_close(handler);
+
+    if (org_data) free(org_data);
+    if (pcm_data) free(pcm_data);
+}
+
+void WEAtoolsPlay() {
+    // use a buffer to hold one period
+    size_t buffer_size = header.bits_per_sample / 8 * header.channels * header.sample_size;
+    char *buffer = buffer = (char*)malloc(buffer_size);
+
+    while (1) {
+        while (WEAtoolsReadDataFromPcm(buffer, buffer_size) == 0) {
+            snd_pcm_writei(handler, buffer, header.sample_size);
+        }
+        // DI("bgm end");
+    }
+    free(buffer);
+}
---
+++ src/atools.h
@@ -0,0 +1,69 @@
+#ifndef _ATOOLS_H
+#define _ATOOLS_H
+#include <pthread.h>
+#include <alsa/asoundlib.h>
+#define ALSA_PCM_NEW_HW_PARAMS_API
+
+// WAVE file header format
+typedef struct WAVE_HEADER_T {
+    char riff[4];                      // RIFF string
+    unsigned int overall_size;         // overall size of file in bytes
+    char wave[4];                      // WAVE string
+    char fmt_chunk_marker[4];          // fmt string with trailing null char
+    unsigned int length_of_fmt;        // length of the format data
+    unsigned int format_type;          // format type. see wave_format_t
+    unsigned int channels;             // number of channels
+    unsigned int sample_rate;          // sampling rate (blocks per second)
+    unsigned int byterate;             // SampleRate * NumChannels * BitsPerSample/8
+    unsigned int block_align;          // NumChannels * BitsPerSample/8
+    unsigned int bits_per_sample;      // bits per sample, 8- 8bits, 16- 16 bits etc
+    char data_chunk_header [4];        // data string or list string
+    unsigned int data_size;            // data size
+} wave_header_t;
+
+typedef enum AUDIO_TYPE_T {
+    WAVE, MP3, FLAC, ALAC, ACC,
+} audio_type_t;
+
+typedef enum WAVE_FORMAT_T {
+    UNKNOWN        = 0,
+    PCM,
+    MS_ADPCM,
+    A_LAW          = 0x6,
+    MU_LAW,
+    IMA_ADPCM      = 0x11,
+    ITU_G723_ADPCM = 0x16,
+    ITU_G721_ADPCM = 0x31,
+    MPEG           = 0x50,
+    EXPERIMENTAL   = 0xffff
+
+} wave_format_t;
+
+typedef struct AUDIO_HEADER_T {
+    union {
+        wave_header_t wave_header;
+    } h;
+    audio_type_t type;
+    long num_samples;
+    size_t sample_size;
+    double duration;
+    unsigned int channels;
+    unsigned int data_rate;
+    unsigned int sample_rate;
+    unsigned int bits_per_sample;
+    size_t data_size;
+} audio_header_t;
+
+#ifdef LITTLE_ENDIAN
+#define BUF2NUM2(buf) ((buf)[0] | ((buf)[1] << 8))
+#define BUF2NUM4(buf) ((BUF2NUM2((buf))) | (BUF2NUM2((buf)+2) << 16))
+#else
+#define BUF2NUM2(buf) ((buf)[1] | ((buf)[0] << 8))
+#define BUF2NUM4(buf) ((BUF2NUM2((buf+2))) | (BUF2NUM2((buf)) << 16))
+#endif
+
+void WEAtoolsDestruct();
+void WEAtoolsPlay();
+void WEAtoolsInit(char *filename);
+#endif
+
